8f94fd2ed1e22f0990d20514685203a6b8f8b044
doann <doann@COSIMA>
2023-03-10 14:46:43 +0700

    Add project files.

---
+++ Controllers/CoursesControllerController.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesControllerController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesControllerController() 
+        {
+            _dbContext = new ApplicationDbContext();        
+        }
+        // GET: CoursesController
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string Name { get; set; }
+    }
+}
